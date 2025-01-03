2afac89d80f67ee67aebf207ff0a12744e6b9470
Avazkhon <avazkhonnazirov33@gmail.com>
2025-01-03 20:19:37 +0500

    edited CourseRepository

--- src/main/java/uz/mu/lms/repository/CourseGroupRepository.java
+++ src/main/java/uz/mu/lms/repository/CourseGroupRepository.java
@@ -1,12 +1,8 @@
 package uz.mu.lms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uz.mu.lms.model.CourseGroup;
-import uz.mu.lms.projection.StudentCourseProjection;
-
-import java.util.List;
 
 @Repository
 public interface CourseGroupRepository extends JpaRepository<CourseGroup, Integer> {
